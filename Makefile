dec9f1c6e1c7933b847d79c5d0335a904c5214ce
Azamat Rakhim <azamatrakhim8@gmail.com>
2026-02-27 17:21:22 +0500

    Add PendSV context switch handler in assembly
    =Implement pendsv_handler to save/restore task register state
    =Save r4-r11 manually onto current task stack (hardware saves r0-r3,r12,LR,PC,xPSR)
    =Store/restore PSP from TCB sp field to switch task stacks
    =Add pendsv.s to Makefile build

--- Makefile
+++ Makefile
@@ -2,11 +2,12 @@ CC = arm-none-eabi-gcc
 CFLAGS = -mcpu=cortex-m3 -mthumb -g -Wall -I -ffreestanding
 LDFLAGS = -nostdlib -T linker.ld -lnosys
 SRC = startup.c main.c kernel.c
+ASM = pendsv.s
 TARGET = kernel.elf
 
 
 $(TARGET): $(SRC)
-	$(CC) $(CFLAGS) $(LDFLAGS) -o $@ $^
+	$(CC) $(CFLAGS) $(LDFLAGS) -o $@ $^ $(ASM)
 
 run: $(TARGET)
 	qemu-system-arm -machine lm3s6965evb -nographic -kernel $(TARGET)
---
+++ pendsv.s
@@ -0,0 +1,44 @@
+.syntax unified
+.cpu cortex-m3
+.thumb
+
+.global pendsv_handler
+.type pendsv_handler, %function
+
+pendsv_handler:
+    MRS R0, PSP /* 1. get current task's stack pointer */
+    STMDB R0!, {R4-R11} /* 2. save r4-r11 onto current task's stack */
+
+    /* 3. save PSP into tasks[current_task].sp */
+    LDR R1, =tasks
+    LDR R2, =current_task
+    LDR R2, [R2]
+
+    /* each TCB is (1 + 128) * 4 = 516 bytes */
+    MOV R3, #516
+    MUL R2, R2, R3
+    ADD R1, R1, R2
+    ADD R1, R2
+    STR R0, [R1]
+
+    /* 4. call scheduler() to update current_task */
+    /* already called from SysTick so skip here */
+
+
+    /* 5. load next task's sp into R0 */
+    LDR R1, =tasks
+    LDR R2, =current_task
+    LDR R2, [R2]
+    MOV R3, #516
+    MUL R2, R2, R3
+    ADD R1, R1, R2
+    LDR R0, [R1]
+
+    /* restore r4-r11 from next task's stack */
+    LDMIA R0!, {R4-R11}
+
+    /* 7. set PSP to next task's stack pointer */
+    MSR PSP, R0
+
+    /* return - CPU auto-restores r0-r3,r12,LR,PC,xPSR */
+    BX LR
