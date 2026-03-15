312b58cc769105646ec50a90cee2354b97d9f902
Azamat Rakhim <azamatrakhim8@gmail.com>
2026-03-15 23:59:19 +0500

    add task sleep and wake [incomplete]

--- kernel.c
+++ kernel.c
@@ -10,6 +10,11 @@ void scheduler(void)
     current_task = (current_task + 1) % task_count;
 }
 
+void task_sleep(uint32_t ms)
+{
+    tasks[current_task].state = TASK_SLEEPING;
+    tasks[current_task].wake_tick;
+}
 
 void kernel_start(void)
 {
--- kernel.h
+++ kernel.h
@@ -1,6 +1,8 @@
 #include <stdint.h>
 
 #define MAX_TASKS 3
+#define TASK_READY 0
+#define TASK_SLEEPING 1
 
 extern int current_task;
 extern int task_count;
@@ -9,6 +11,8 @@ extern int kernel_started;
 typedef struct {
     uint32_t *sp;
     uint32_t stack[128];
+    uint8_t state;
+    uint32_t wake_tick;
 } TCB;
 extern TCB tasks[MAX_TASKS];
 void create_task(TCB *task, void (*entry)(void));
